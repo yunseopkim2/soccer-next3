395475cf4308ab983c62a8bd00f46717c8adf8b0
yunseopkim2 <dbstjqdlwksj@gmail.com>
2022-04-28 14:22:49 +0900

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import '@/styles/globals.css'
+import { wrapper } from '@/modules/store'
+import {Header, Nav, Footer} from '@/components'
+
+const App = ({ Component, pageProps }) => {
+  return(<div>
+  <Nav/>
+       <Header/>
+    <Component {...pageProps} />
+    <Footer class = "footer"/>
+  </div>
+  )
+  
+}
+
+
+export default wrapper.withRedux(App)
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { Header, Layout, Modal, Pagination, Nav, Table, Footer } from '@/components'
+
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <Layout/>
+     
+  )
+}
